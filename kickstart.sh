64e2799ab33cd12e20e247594ecc45554873a655
ShaunB <pictowolf@gmail.com>
2022-03-12 16:39:17 +0000

    Quote in wrong place

--- kickstart.sh
+++ kickstart.sh
@@ -10,4 +10,4 @@ sudo usermod -aG wheel $user
 
 echo "The IP address is: "$(ip -f inet addr show eth1 | sed -En -e 's/.*inet ([0-9.]+).*/\1/p')
 echo "The username is: $user"
-echo "The generated password is: "$password
+echo "The generated password is: $password"
